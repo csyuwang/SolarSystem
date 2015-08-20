f1c8a0697e7014d69b090775b3d5e21464f915ef
csWangYu <370196867@qq.com>
2015-08-20 17:08:43 +0800

    create a basic earth model

---
+++ libs/earth-basic.js
@@ -0,0 +1,49 @@
+// 继承自Sim.App的地球应用EarthApp
+EarthApp = function () {
+    Sim.App.call(this);
+}
+// 子类EarthApp
+EarthApp.prototype = new Sim.App();
+
+EarthApp.prototype.init = function (param) {
+    //调用父类Sim.App的初始化方法，设置场景，渲染器，和默认相机
+    Sim.App.prototype.init.call(this, param);
+    // 初始化地球物体并添加到EarthApp中
+    var earth = new Earth();
+    earth.init();
+
+    this.addObject(earth);
+
+}
+
+// 地球物体
+Earth = function () {
+    Sim.Object.call(this);
+}
+
+Earth.prototype = new Sim.Object();
+
+Earth.prototype.init = function () {
+
+    // 创建纹理，材质，几何体，生成网格，设置Object3D
+    var earthMap = "images/earth_surface_2048.jpg";
+    var texture = new THREE.ImageUtils.loadTexture(earthMap);
+    var material = new THREE.MeshBasicMaterial({map: texture});
+    var geometry = new THREE.SphereGeometry(1, 32, 32);
+    var mesh = new THREE.Mesh(geometry, material);
+
+    mesh.rotation.z = Earth.TILT;
+
+    this.setObject3D(mesh);
+}
+
+Earth.prototype.update = function () {
+    // 旋转
+    this.object3D.rotation.y += Earth.ROTATION_Y;
+}
+// 每次旋转弧度
+Earth.ROTATION_Y = 0.0025;
+// z轴初始倾斜
+Earth.TILT = 0.41;
+
+
